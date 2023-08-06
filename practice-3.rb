e33c00495565d7d6fc0d46971c345f10e03646d2
EC2 Default User <130027255+satoikokoro@users.noreply.github.com>
2023-08-06 09:17:52 +0000

    230805練習

---
+++ practice-3.rb
@@ -0,0 +1,10 @@
+#2つの整数値の入力を受け付け、それらの掛け算の結果を出力するプログラム
+
+puts "計算を始めます"
+puts "2つの値を入力してください"
+first_key = gets.to_i
+second_key = gets.to_i
+
+puts "計算結果を出力します"
+puts "#{first_key}*#{second_key}=#{first_key * second_key}"
+puts "計算を終了します"
