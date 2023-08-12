128937bb572e0a6431ca6c832a085a7dc4c4c05a
EC2 Default User <130027255+satoikokoro@users.noreply.github.com>
2023-08-12 13:26:38 +0000

    230812

--- practice-9.rb
+++ practice-9.rb
@@ -1,7 +1,25 @@
-#カレンダー作成問題
-
 require 'date'
 
 head = Date.today.strftime("%B %Y")
-year  = Date.today.year
+year = Date.today.year
 mon = Date.today.mon
+firstday_wday = Date.new(year, mon, 1).wday
+lastday_date = Date.new(year, mon, -1).day
+week = %w(日 月 火 水 木 金 土)
+
+puts head.center(20)
+puts week.join(" ")
+print "　　" * firstday_wday
+
+wday = firstday_wday
+(1..lastday_date).each do |date|
+    print date.to_s.rjust(2) + " "
+    wday = wday + 1
+    if wday % 7 == 0
+        print "\n"
+    end
+end
+
+if wday % 7 != 0
+    print "\n"
+end
