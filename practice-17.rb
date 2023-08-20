b21095a4febab455e3e523a0b2a0ecc0914093c5
EC2 Default User <130027255+satoikokoro@users.noreply.github.com>
2023-08-20 11:44:45 +0000

    230820

---
+++ practice-17.rb
@@ -0,0 +1,11 @@
+def keisan(num1,num2)
+  puts "#{num1}と#{num2}をかけた答えは#{num1 * num2}です"
+end 
+
+puts "最初の数字を入力してください"
+num1 = gets.to_i
+
+puts "2番目の数字を入力してください"
+num2 = gets.to_i
+
+keisan(num1,num2)
